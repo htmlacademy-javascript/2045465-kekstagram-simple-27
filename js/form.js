070e132554aa32cb63e918a628ba4f1f4dd601f4
Инна Дементьева <acorp.murka@gmail.com>
2022-10-29 22:46:02 +0300

    Добавляет настройку эффектов и форму редактирования

--- js/form.js
+++ js/form.js
@@ -5,31 +5,59 @@ const buttonCancel = document.querySelector('#upload-cancel');
 const imageLoading = document.querySelector ('.img-upload__overlay');
 const comment = document.querySelector('.text__description');
 const imageForm = document.querySelector('.img-upload__form');
+const buttonSubmit = document.querySelector('.img-upload__submit');
+const buttonSmaller = document.querySelector('.scale__control--smaller');
+const buttonBigger = document.querySelector('.scale__control--bigger');
+const scaleValue = document.querySelector('.scale__control--value');
+const imagePreview = document.querySelector('.img-upload__preview');
+const effectsList = document.querySelector('.effects__list');
+
+// Переменные для масштабирования
+const minScale = 25;
+const maxScale = 100;
+let userScale = 100;
+
+// Переменная для хранения текушего эффекта
+let effectsPreview; // сначала эффект пустой
+
+//Поле в фокусе - esc не работает
+const isFieldFocused = () =>
+  document.activeElement === comment;
 
 const onModalEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !isFieldFocused()) {
     evt.preventDefault();
     closeUserModal ();
   }
 };
 
+//ф-ция возвращает данные к исходному состоянию
+const setDefaultForm = () =>{
+  imagePreview.classList.remove(effectsPreview);
+  scaleValue.value = '100 %';
+  imagePreview.style.transform = 'scale(1)';
+  comment.value = '';
+};
+
 function openUserModal () {
+  // Показать окно
   imageLoading.classList.remove('hidden');
   body.classList.add('modal-open');
-
+  // Добавить обработчик закрытия
   document.addEventListener('keydown', onModalEscKeydown);
+  buttonSubmit.setAttribute('disabled', 'disabled');
 }
 
 function closeUserModal () {
+  // Скрыть окно
   imageLoading.classList.add('hidden');
   body.classList.remove('modal-open');
-  loadingFile.value = ''; //???
-  comment.value = '';
-
+  // Удалить обработчик
   document.removeEventListener('keydown', onModalEscKeydown);
+  setDefaultForm();
 }
 
-loadingFile.addEventListener('click', () => {
+loadingFile.addEventListener('change', () => {
   openUserModal ();
 });
 
@@ -37,13 +65,47 @@ buttonCancel.addEventListener('click', () => {
   closeUserModal ();
 });
 
+const scaleSmaller = () => {
+  if(userScale > minScale) {
+    userScale -= minScale;
+    scaleValue.value = `${userScale} %`;
+    imagePreview.style.transform = `scale(${userScale / 100})`;
+  }
+};
+
+const scaleBigger = () => {
+  if(userScale < maxScale) {
+    userScale += minScale;
+    scaleValue.value = `${userScale} %`;
+    imagePreview.style.transform = `scale(${userScale / 100})`;
+  }
+};
+
+buttonSmaller.addEventListener('click', () => {
+  scaleSmaller();
+});
+
+buttonBigger.addEventListener('click', () => {
+  scaleBigger();
+});
+
+effectsList.addEventListener('change', (evt) => {
+  imagePreview.classList.remove(effectsPreview);
+  const overlayEffect = `effects__preview--${evt.target.value}`;
+  imagePreview.classList.add(overlayEffect);
+  effectsPreview = overlayEffect;
+});
+
+//Валидация
+
 const pristine = new Pristine(imageForm);
 
-imageForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-
+// Проверка на ввод в поле коментария
+imageForm.addEventListener('input', () => {
   const isValid = pristine.validate();
-  if (!isValid) {
-    evt.preventDefault();
+  if (isValid) {
+    buttonSubmit.removeAttribute('disabled');
+  } else {
+    buttonSubmit.setAttribute('disabled', 'disabled');
   }
 });
--- js/main.js
+++ js/main.js
@@ -2,5 +2,4 @@ import {getPhotoMiniature} from './miniature.js';
 import {createPhotos} from './data.js';
 import {SIMILAR_PHOTO_COUNT} from './data.js';
 import './form.js';
-//import './edit-form.js';
 getPhotoMiniature(createPhotos(SIMILAR_PHOTO_COUNT));
