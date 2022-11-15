3aa96295fe42cc8c6e2a1afad62686188437fa42
Инна Дементьева <acorp.murka@gmail.com>
2022-11-16 00:16:44 +0300

    Добавляет загрузку выбранного файла и исправляет названия констант

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {renderPhotoMiniature} from './miniature.js';
 import {getData} from './api.js';
 import {setUserFormSubmit} from './form.js';
+import './upload-preview.js';
 
 getData((images)=> {
   renderPhotoMiniature(images);
